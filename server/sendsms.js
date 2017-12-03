dc7a8ae92aca12cdc10313fb903ed28523e486d9
Jacob <jphill0808@gmail.com>
2017-12-02 20:42:12 -0500

    change variable name

--- server/sendsms.js
+++ server/sendsms.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
-
-const sendMessage = () => {
+const sendMessage = (userPhoneNumber) => {
   client.messages.create({
-  to: '+16784295786',
+  to: `+1{userPhoneNumber}`,
   from: process.env.FROM_PHONE_NUMBER,
   body: 'yo live long and prosper css master'
   })
@@ -18,11 +17,15 @@ const sendMessage = () => {
   })
 }
 
+const testFunc = (hi) =>{
+  console.log('testFunc parameter: ', hi);
+}
+
 let today = new Date();
 let time = today.getHours();
 console.log(time);
 
-
 module.exports = {
-  sendMessage: sendMessage
+  sendMessage: sendMessage,
+  testFunc: testFunc
 }
