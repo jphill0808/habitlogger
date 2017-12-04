2d486b7b0d3689ae0a3ebbf0d1028e1c83bac39a
Jacob <jphill0808@gmail.com>
2017-12-03 21:41:10 -0500

    implement cron message

--- server/cronMessage.js
+++ server/cronMessage.js
@@ -2,39 +2,35 @@ const db = require('../db/index.js');
 const CronJob = require('cron').CronJob;
 const sendMessage = require('./sendsms.js').sendMessage;
 
-const compareTimeAndSend = (deadline, now, phoneNumb) => {
-  let timeLeft = (deadline - now)/(1000 * 60);
-  if(timeLeft <= 1440){// minutes of a day
-    // sendMessage(phoneNumb)
-    console.log('message sent!')
+const compareTimeAndSend = (user, now, cb) => {
+  let timeLeft = (user.deadline - now)/(1000 * 60);
+  if(timeLeft <= 1440 && timeLeft >= 0){ // less than 1 day(minutes) left && not overdue
+    sendMessage(user.phoneNumb, user.habit, timeLeft)
+    cb(user.habit, user.username)
   }
 }
 
-const job = new CronJob({
+const sendMessageCron = new CronJob({
   cronTime: '* * * * * *',
   onTick: function(){
     db.getInfo((list) => {
-      // console.log('list: ', list)
+      console.log('data:::::::: ', list)
       list.forEach((user) => {
-        console.log('user: ', user)
         const deadline = user.deadline;
-        // console.log('deadline: ', deadline)
         let now = Date.now();
-        if(!user.messageSent){
-          compareTimeAndSend(deadline, now, user.phoneNumb);
-          // user.messageSent = true;
-          //db.updateMessage(user.habit);
+        if(user.messageSent === false){
+          compareTimeAndSend(user, now, (habit, name)=>{
+           db.updateMessage(habit, name);
+          });
         }
       })
     })
-    // console.log('this is on tick');
   },
   start: false,
 });
-job.start();
 
-console.log('jobstatus: ', job.running);
+// sendMessageCron.start();
 
 exports.module = {
-  job,
+  sendMessageCron,
 }
